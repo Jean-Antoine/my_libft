c2ae780b45831288e562eeb7d972d32dd87df6c1
Jean-antoine Da silva <jeada-si@bess-f3r1s9.clusters.42paris.fr>
2024-01-16 13:33:48 +0100

    fix min_array and max_array

--- srcs/base/ft_max_array.c
+++ srcs/base/ft_max_array.c
@@ -6,7 +6,7 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 18:15:26 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/01/15 18:17:56 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/01/16 13:33:28 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_max_array(int *n, int size)
 	int	i;
 	int	max;
 
-	if (size)
-		return (*n);
 	i = 1;
 	max = *n;
 	while (i < size)
--- srcs/base/ft_min_array.c
+++ srcs/base/ft_min_array.c
@@ -6,7 +6,7 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 09:10:43 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/01/16 09:12:07 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/01/16 13:33:11 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	ft_min_array(int *n, int size)
 	int	i;
 	int	min;
 
-	if (size)
-		return (*n);
 	i = 1;
 	min = *n;
 	while (i < size)
