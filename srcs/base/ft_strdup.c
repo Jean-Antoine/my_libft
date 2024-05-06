fec466a76b5e73c2c287f4eaed88d87794271c54
Jean-antoine Da silva <jeada-si@bess-f3r1s5.clusters.42paris.fr>
2024-05-06 17:30:40 +0200

    norme

--- srcs/base/ft_strdup.c
+++ srcs/base/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 09:30:01 by jeada-si          #+#    #+#             */
-/*   Updated: 2023/11/15 14:10:18 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/04/12 16:00:18 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strdup(const char *s)
 	size_t	length;
 
 	length = ft_strlen(s);
-	out = (char *)ft_calloc(length + 1, sizeof(char));
+	out = (char *) ft_calloc(length + 1, sizeof(char));
 	if (!out)
 		return (NULL);
 	ft_memcpy(out, s, length);
